2238540c81c3c6e0dd526e2891291bc436d62732
KeyMarmot <127695565+KeyMarmot@users.noreply.github.com>
2026-02-24 11:27:08 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+
+Conversation opened. 1 unread message.
+
+Skip to content
+Using Trinity Alps Unified School District Mail with screen readers
+
+1 of 523
+(no subject)
+Inbox
+
+Brodie Brower <35135@tausd.org>
+9:20 AM (2 hours ago)
+to me
+
 console.log('Script Starting...')
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
@@ -7,6 +21,16 @@ document.addEventListener('keydown', handleKeyPress);
 
 function handleKeyPress(e){
     console.log(e.key);
+
+    if (e.key == 'ArrowLeft'){
+        //move left
+        paddle.x -= paddle.hspeed;
+    }
+    else if (e.key == 'ArrowRight'){
+        //move right
+        paddle.x += paddle.hspeed;
+    }
+}
 
 
 function drawCircle(x, y, radius, fill, stroke, startAngle=0, endAngle=360){
@@ -22,7 +46,7 @@ function drawCircle(x, y, radius, fill, stroke, startAngle=0, endAngle=360){
 const circle = {
     x: 32, 
     y: 32,
-    radius: 32,
+    radius: 16,
     haccelerate: 1,
     vaccelerate: 1,
     hspeed: 1,
@@ -33,14 +57,23 @@ const circle = {
 
 
 function moveWithBounce(circle){
-    circle.hspeed += circle.haccelerate;
-    circle.vspeed += circle.vaccelerate;
+    circle.hspeed += circle.haccelerate
+    circle.vspeed += circle.vaccelerate
     circle.x += circle.hspeed;
     circle.y += circle.vspeed;
 
     if (circle.x-circle.radius < 0 || circle.x+circle.radius > canvas.width){
         circle.hspeed *= -1;
     }
+
+    if (circle.x-circle.radius < 0 || circle.x+circle.radius > canvas.width){
+        circle.haccelerate *= -1;
+    }
+
+    if (circle.y-circle.radius < 0 || circle.y+circle.radius > canvas.height){
+        circle.vaccelerate *= -1;
+    }
+
 
     if (circle.y-circle.radius < 0 || circle.y+circle.radius > canvas.height){
         circle.vspeed *= -1;
@@ -60,8 +93,4 @@ function drawLoop(){
     requestAnimationFrame(drawLoop);
 }
 
-
 drawLoop();
-
-
-
