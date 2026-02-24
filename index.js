d503871ed95bae795f92063c301842f4c4e29783
KeyMarmot <127695565+KeyMarmot@users.noreply.github.com>
2026-02-24 11:25:43 -0800

    Update index.js

--- index.js
+++ index.js
@@ -8,16 +8,6 @@ document.addEventListener('keydown', handleKeyPress);
 function handleKeyPress(e){
     console.log(e.key);
 
-    if (e.key == 'ArrowLeft'){
-        //move left
-        paddle.x -= paddle.hspeed;
-    }
-    else if (e.key == 'ArrowRight'){
-        //move right
-        paddle.x += paddle.hspeed;
-    }
-}
-
 
 function drawCircle(x, y, radius, fill, stroke, startAngle=0, endAngle=360){
     ctx.fillStyle = fill;
@@ -33,23 +23,19 @@ const circle = {
     x: 32, 
     y: 32,
     radius: 32,
-    hspeed:2,
-    vspeed: 2,
+    haccelerate: 1,
+    vaccelerate: 1,
+    hspeed: 1,
+    vspeed: 1,
     fill: 'green',
     stroke: 'green'
 }
 
-const paddle =  {
-    x: canvas.width/2,
-    y: 550,
-    width: 128,
-    height: 32,
-    hspeed: 8,
-    color: 'black'
-}
 
 function moveWithBounce(circle){
-        circle.x += circle.hspeed;
+    circle.hspeed += circle.haccelerate;
+    circle.vspeed += circle.vaccelerate;
+    circle.x += circle.hspeed;
     circle.y += circle.vspeed;
 
     if (circle.x-circle.radius < 0 || circle.x+circle.radius > canvas.width){
@@ -62,10 +48,6 @@ function moveWithBounce(circle){
 
 }
 
-function drawRectObj(rectObj){
-    ctx.fillStyle = rectObj.color,
-    ctx.fillRect(rectObj.x, rectObj.y, rectObj.width, rectObj.height)
-}
 function drawLoop(){
     //clear canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -74,12 +56,12 @@ function drawLoop(){
 
     //draw objects
     drawCircle(circle.x, circle.y, circle.radius, circle.fill, circle.stroke);
-    drawRectObj(paddle);
     //call drawLoop
     requestAnimationFrame(drawLoop);
 }
 
 
 drawLoop();
+
 
 
