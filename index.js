5d5406396a0cee38f07bb4d186dccfd7cca1a558
KeyMarmot <127695565+KeyMarmot@users.noreply.github.com>
2026-03-03 08:59:29 -0800

    added text box control

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+
 console.log('Script Starting...')
 const canvas = document.getElementById('canvas');
 const Changer = document.getElementById('AttChange');
 const ctx = canvas.getContext('2d');
 const hAccel = document.getElementById('hAccel');
+const vAccel = document.getElementById('vAccel');
+const hspeed = document.getElementById('hSpeed');
+const vspeed = document.getElementById('vSpeed');
+const color = document.getElementById('color');
+const size = document.getElementById('size');
+const startPosx = document.getElementById('startingPosx');
+const startPosy = document.getElementById('startingPosy')
+let FrameCount = 1;
 
 document.addEventListener('keydown', handleKeyPress);
 
@@ -10,17 +19,15 @@ function handleKeyPress(e){
     console.log(e.key);
 }
 
-function drawCircle(x, y, radius, fill, stroke, startAngle=0, endAngle=360){
+function drawCircle(x, y, radius, fill, startAngle=0, endAngle=360){
     ctx.fillStyle = fill;
-    ctx.strokeStyle = stroke;
     ctx.beginPath();
     ctx.arc(x, y, radius, startAngle, endAngle);
     ctx.fill();
-    ctx.stroke();
 }
 
 
-const circle = {
+let circle = {
     x: 32, 
     y: 32,
     radius: 16,
@@ -28,13 +35,12 @@ const circle = {
     vaccelerate: 1,
     hspeed: 1,
     vspeed: 1,
-    fill: 'green',
-    stroke: 'green'
+    fill: 'green'
 }
 
 
 function moveWithBounce(circle){
-    console.log(circle.vspeed);
+    //console.log(circle.vspeed);
     circle.hspeed += circle.haccelerate;
     circle.vspeed += circle.vaccelerate;
     circle.x += circle.hspeed;
@@ -55,18 +61,21 @@ function moveWithBounce(circle){
     if (circle.y-circle.radius < 0 || circle.y+circle.radius > canvas.height){
         circle.vspeed *= -1;
     }
-
 }
 
-function changeAttributes(){
+function changeAttributes(circle){
     //stop animation
-    cancelAnimationFrame(drawLoop);
-    //clear current attributes
-    
-    //append new attributes
-
-    //continue animation
-    drawLoop();
+    //append new properties
+    circle.vaccelerate = Number(vAccel.value);
+    circle.vspeed = Number(vspeed.value);
+    circle.hspeed = Number(hspeed.value);
+    circle.radius = Number(size.value);
+    circle.x = Number(startPosx.value);
+    circle.y = Number(startPosy.value);
+    circle.haccelerate = Number(hAccel.value);
+    circle.fill = color.value;
+    console.log(circle)
+    //continue 
 }
 
 function drawLoop(){
@@ -74,25 +83,15 @@ function drawLoop(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     //update
     moveWithBounce(circle);
-
     //draw objects
-    drawCircle(circle.x, circle.y, circle.radius, circle.fill, circle.stroke);
+    drawCircle(circle.x, circle.y, circle.radius, circle.fill);
     //call drawLoop
     requestAnimationFrame(drawLoop);
 }
 
-
-Changer.onclick = changeAttributes;
+Changer.onclick = () => changeAttributes(circle);
 
 drawLoop();
-
-
-
-
-
-
-
-
 
 
 
