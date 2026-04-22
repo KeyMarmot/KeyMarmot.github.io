388baf11be0eb2a6971f25e25af6c2451b3daecc
KeyMarmot <127695565+KeyMarmot@users.noreply.github.com>
2026-04-22 10:35:45 -0700

    start setup for 3d rendering

--- index.js
+++ index.js
@@ -2,3 +2,20 @@
 console.log('Script Starting...')
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+
+var sampleRect = {
+  x: 10,
+  y: 10,
+  z: 10,
+  w: 10,
+  h: 10,
+  d: 10,
+  hor: 0,
+  vert: 0,
+}
+
+function drawCube(mol) = {
+  ctx drawrect (mol.x, mol.y, mol.w, mol.h)
+}
+
+drawCube(sampleRect)
