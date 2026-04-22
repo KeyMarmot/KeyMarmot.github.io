ff33c850a5454132f96a066adcb7b11b64c8813b
KeyMarmot <127695565+KeyMarmot@users.noreply.github.com>
2026-04-22 11:55:06 -0700

    work on environment.js

--- environment.js
+++ environment.js
@@ -4,20 +4,29 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 var sampleRect = {
-  x: 10,
-  y: 10,
-  z: 10,
-  w: 10,
-  h: 10,
-  d: 10,
-  hor: 0,
+  x: 50,
+  y: 50,
+  z: 50,
+  w: 50,
+  h: 50,
+  d: 50,
+  hor: 3*Math.PI/2,
   vert: 0,
   color: "blue",
+}
+
+const camera = {
+  x: 540,
+  y: 300,
+  z: 0,
+  hor: 0,
+  vert:0,
 }
 
 function drawCube(mol) {
   ctx.fillStyle = mol.color;
   ctx.fillRect (mol.x, mol.y, mol.w, mol.h);
+  ctx.fillRect (mol.x, mol.y, mol.w, mol.d);
 }
 
 drawCube(sampleRect)
