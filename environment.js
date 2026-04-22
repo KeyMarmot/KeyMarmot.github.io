32eb46b0072df9a39dc30881eb8918b9620d6550
KeyMarmot <127695565+KeyMarmot@users.noreply.github.com>
2026-04-22 14:51:24 -0700

    dialing in

--- environment.js
+++ environment.js
@@ -59,7 +59,7 @@ function drawCube(mol) {
   ctx.fillStyle = mol.color;
   ctx.beginPath();       // Start a new path
   ctx.moveTo(mol.p1.x * 50, mol.p1.y * 50);    // Starting point (x=50, y=50)
-  ctx.lineTo(mol.p2.x * 50, mol.p2.x * 50);  // Ending point (x=200, y=150)
+  ctx.lineTo(mol.p2.x * 50, mol.p3.x * 50);  // Ending point (x=200, y=150)
   ctx.stroke();          // Render the line
   ctx.fillRect (mol.x, mol.y, mol.w, mol.h);
 }
