fc2ebc614c495b61609d4fc4bc87cbae60a8c8a9
KeyMarmot <127695565+KeyMarmot@users.noreply.github.com>
2026-04-22 12:18:28 -0700

    rewriting how information is stored

--- environment.js
+++ environment.js
@@ -4,14 +4,46 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 var sampleRect = {
-  x: 50,
-  y: 50,
-  z: 50,
-  w: 50,
-  h: 50,
-  d: 50,
-  hor: 3*Math.PI/2,
-  vert: 0,
+  p1 :{
+    x:1,
+    y:1,
+    z:1,
+  },
+  p2:{
+    x:-1,
+    y:1,
+    z:1,
+  },
+  p3:{
+    x:-1,
+    y:-1,
+    z:1,
+  },
+  p4:{
+    x:-1,
+    y:1,
+    z:-1,
+  },
+  p5:{
+    x:-1,
+    y:-1,
+    z:-1,
+  },
+  p6:{
+    x:1,
+    y:-1,
+    z:1,
+  },
+  p7:{
+    x:1,
+    y:-1,
+    z:-1,
+  },
+  p8:{
+    x:1,
+    y:1,
+    z:-1,
+  },
   color: "blue",
 }
 
@@ -25,8 +57,8 @@ const camera = {
 
 function drawCube(mol) {
   ctx.fillStyle = mol.color;
+  mol.x = 
   ctx.fillRect (mol.x, mol.y, mol.w, mol.h);
-  ctx.fillRect (mol.x, mol.y, mol.w, mol.d);
 }
 
 drawCube(sampleRect)
