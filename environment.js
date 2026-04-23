0e418689e122472f51dba49eeb48aebd9b3c4730
KeyMarmot <127695565+KeyMarmot@users.noreply.github.com>
2026-04-23 11:42:32 -0700

    fine tuning

--- environment.js
+++ environment.js
@@ -72,6 +72,9 @@ function drawWireFrame(mol) {
   ctx.fillStyle = 'grey';
   ctx.beginPath();
   ctx.arc(mol.p1.x, mol.p1.y, 3, 0, 2 * Math.PI);
+  ctx.arc(mol.p2.x, mol.p2.y, 3, 0, 2 * Math.PI);
+  ctx.arc(mol.p3.x, mol.p3.y, 3, 0, 2 * Math.PI);
+  ctx.arc(mol.p6.x, mol.p6.y, 3, 0, 2 * Math.PI);
   ctx.fill();
 }
 
